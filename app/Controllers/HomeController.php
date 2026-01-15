dd4d18f155a8a8c68267881b346bd5f9f470c9f0
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-15 02:00:14 +0000

    14/01/2026 : Ajout HomeController

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+// app/Controllers/HomeController.php
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Models\Vehicule;
+use App\Models\Categorie;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $vehicules = Vehicule::listerVehicule(4, 0);
+        $categories = Categorie::listerCategorie();
+        
+        if (!$vehicules) {
+            $vehicules = [];
+        }
+        
+        if (!$categories) {
+            $categories = [];
+        }
+        
+        $this->view('home', [
+            'vehicules' => $vehicules,
+            'categories' => $categories
+        ]);
+    }
+}
