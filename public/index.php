72861e7f519d3e9ab125577b4432afe6eb18ff50
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-15 12:18:52 +0000

    Ajout des routes pour les pages admin dans le router

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Core\Router;
 use App\Controllers\AuthController;
 use App\Controllers\HomeController;
+use App\Controllers\AdminController;
+
 
 session_start();
 
@@ -18,6 +20,13 @@ $router->add('/login_post', AuthController::class, 'login');
 $router->add('/register', AuthController::class, 'registerForm');
 $router->add('/register_post', AuthController::class, 'register');
 $router->add('/logout', AuthController::class, 'logout');
+
+$router->add('/admin/dashboard', AdminController::class, 'dashboard');
+$router->add('/vehicles',AdminController::class,'vehicles');
+$router->add('/categories',AdminController::class,'categories');
+$router->add('/reservations', AdminController::class, 'reservations');
+$router->add('/reviews', AdminController::class, 'reviews');
+
 
 $requestUri = str_replace(BASE_URL, '', $_SERVER['REQUEST_URI']);
 $router->run($requestUri); 
