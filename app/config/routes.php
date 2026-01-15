bf910e038d943bfbf8606e6b0b183e4c1c156e49
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-15 02:00:13 +0000

    14/01/2026 : Mise à jour routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,1 +1,13 @@
- 
+<?php
+
+use App\Controllers\AuthController;
+use App\Controllers\HomeController;
+
+$router->add('/', HomeController::class, 'index');
+
+$router->add('/login', AuthController::class, 'loginForm');
+$router->add('/login_post', AuthController::class, 'login');
+
+$router->add('/register', AuthController::class, 'registerForm');
+$router->add('/register_post', AuthController::class, 'register');
+$router->add('/logout', AuthController::class, 'logout');
