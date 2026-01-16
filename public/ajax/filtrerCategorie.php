d22d8ed32fa814bf37b542f6b569a45544878813
AZIZABADA10 <abadaaziz288@gmail.com>
2026-01-16 10:00:43 +0000

    Amélioration du script Ajax pour le filtrage par catégorie

--- public/ajax/filtrerCategorie.php
+++ public/ajax/filtrerCategorie.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
-use App\Classes\Vehicule;
+use App\Models\Vehicule;
 
 $id = $_GET['id'] ?? 0;
 
